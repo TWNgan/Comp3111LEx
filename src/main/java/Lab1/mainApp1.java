1d35c3e2d204e34a4b8414007650e3a9a79633ba
TWNgan <ngantw1126@gmail.com>
2025-09-19 23:08:43 +0800

    This is an initial commit for Comp3111LEx

---
+++ src/main/java/Lab1/mainApp1.java
@@ -0,0 +1,10 @@
+package Lab1;
+
+public class mainApp1 {
+    public static void main(String[] args) {
+        int b = 2;
+        int e = 5;
+        System.out.println(b + " raised to the power of " + e + " is " + myLibrary.power(b, e));
+        System.out.println(e + "! is " + myLibrary.factorial(e));
+    }
+}
---
+++ src/main/java/Lab1/myLibrary.java
@@ -0,0 +1,19 @@
+package Lab1;
+
+public class myLibrary {
+    public static int power(int base, int exponent) {
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
